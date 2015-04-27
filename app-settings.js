8008ee6554a4f458653d973422c05b53fff9c5b7
Your Name <you@example.com>
2015-04-27 23:40:55 +0300

    ok

---
+++ app-settings.js
@@ -0,0 +1,14 @@
+(function() {
+  window.HauserAppSettings = {
+    apiBaseUrl: "http://hauser-prod.herokuapp.com/api/business/v1",    
+    imagesCdnBaseUrl: "http://hauser-prod.herokuapp.com/images",
+    rootUrlState: "menu.feed",
+    unauthorizedFallbackUrlState: "landing.signin"
+    
+    // apiBaseUrl: "http://localhost:3000/api/business/v1",    
+    // imagesCdnBaseUrl: "http://localhost:3000/images",
+    // rootUrlState: "menu.feed",
+    // unauthorizedFallbackUrlState: "landing.signin"
+  }
+})();
+
